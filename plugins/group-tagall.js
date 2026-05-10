7df61fed5e938a9face2f704145df96a480b24ac
Lyonn <edwardshanti3@gmail.com>
2026-05-09 21:30:25 -0600

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,55 +1,65 @@
 let handler = async (m, { conn, isAdmin, isOwner, text }) => {
-  if (!m.isGroup) return m.reply(`
-ㅤ    ꒰  ㅤ ❌ ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
+  let isGroup = m.chat.endsWith('@g.us')
+
+  if (!isGroup) return m.reply(`
+ㅤ    ꒰  ㅤ ❌ ㅤ *αℓуα ѕυв* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ єяяσя 木 ɢяυρσ ㅤ 性
 
 > ₊· ⫏⫏ ㅤ Sσℓσ єη gяυρσѕ
-
-ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
-  `.trim())
+`.trim())
 
   if (!isAdmin && !isOwner) return m.reply(`
-ㅤ    ꒰  ㅤ ❌ ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
+ㅤ    ꒰  ㅤ ❌ ㅤ *αℓуα ѕυв* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ α∂мιη 木 яєqυєяι∂σ ㅤ 性
 
 > ₊· ⫏⫏ ㅤ Nєcєѕιтαѕ ѕєя α∂мιη
-
-ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
-  `.trim())
+`.trim())
 
   let grupo = await conn.groupMetadata(m.chat)
   let participantes = grupo.participants
-  let mensaje = text || '📢 *Atención* 📢'
+  let mensaje = text || '📢 Atencion'
 
   let listaMenciones = []
   let numeros = ''
 
   for (let i = 0; i < participantes.length; i++) {
     let user = participantes[i].id
+    if (user === conn.user.jid) continue
     listaMenciones.push(user)
     numeros += `> ${i + 1}. @${user.split('@')[0]}\n`
   }
 
   let caption = `
-ㅤ    ꒰  ㅤ 📢 ㅤ *αℓуα - TΛGΛLL* ㅤ ⫏⫏  ꒱
-ㅤ    ⿻ ㅤ ✿ ㅤ мєη¢ισи 木 gяυρσ ㅤ 性
+ㅤ    ꒰  ㅤ 📢 ㅤ *αℓуα ѕυв* ㅤ ⫏⫏  ꒱
+ㅤ    ⿻ ㅤ ✿ ㅤ тαgαℓℓ 木 мєη¢ιση ㅤ 性
 
-> ₊· ⫏⫏ ㅤ *📝 Mensaje:* ${mensaje}
-> ₊· ⫏⫏ ㅤ *👥 Miembros:* ${participantes.length}
+> ₊· ⫏⫏ ㅤ *Mensaje:* ${mensaje}
+> ₊· ⫏⫏ ㅤ *Miembros:* ${participantes.length}
 
 ${numeros}
 
-ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
+ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα ѕυв* ㅤ ⫏⫏ ꒱
   `.trim()
 
-  await conn.sendMessage(m.chat, { text: caption, mentions: listaMenciones }, { quoted: m })
-  await m.react('📢')
+  await conn.sendMessage(m.chat, {
+    text: caption,
+    mentions: listaMenciones,
+    contextInfo: {
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: "120363407253203904@newsletter",
+        newsletterName: "αℓуα - ¢нαηηєℓ",
+        serverMessageId: 1
+      }
+    }
+  }, { quoted: m })
 }
 
 handler.help = ['tagall']
 handler.tags = ['group']
 handler.command = ['tagall', 'todos', 'mencionar']
-handler.desc = 'ᴍᴇɴᴄɪᴏɴᴀ ᴀ ᴛᴏᴅᴏꜱ ʟᴏꜱ ᴍɪᴇᴍʙʀᴏꜱ ᴅᴇʟ ɢʀᴜᴘᴏ
+handler.desc = 'ᴍᴇɴᴄɪᴏɴᴀ ᴀ ᴛᴏᴅᴏꜱ ʟᴏꜱ ᴍɪᴇᴍʙʀᴏꜱ ᴅᴇʟ ɢʀᴜᴘᴏ'
 handler.group = true
 handler.admin = true
 
