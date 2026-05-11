2adb87c27f64ef71a83b224dca5464895de8186f
Lyonn <edwardshanti3@gmail.com>
2026-05-10 21:00:33 -0600

    Actualizar group-close.js

--- plugins/group-close.js
+++ plugins/group-close.js
@@ -1,19 +1,29 @@
-const handler = async (m, { conn }) => {
-  if (!m.isGroup) return m.reply('> ₊· ⫏⫏ ㅤ *єѕтє ¢σмαη∂σ ѕσℓσ ƒυη¢ισηα єη gяυρσѕ*')
+let handler = async (m, { conn, isAdmin, isOwner, isROwner, isBotAdmin }) => {
+  if (!m.chat.endsWith('@g.us')) return m.reply(`
+ㅤ    ꒰  ㅤ ❌ ㅤ *αℓуα ѕυв* ㅤ ⫏⫏  ꒱
+ㅤ    ⿻ ㅤ ✿ ㅤ єяяσя 木 ɢяυρσ ㅤ 性
 
-  const groupMetadata = await conn.groupMetadata(m.chat)
-  const participants = groupMetadata.participants
-  const botId = conn.user.jid.split(':')[0] + '@s.whatsapp.net'
-  const senderId = m.sender.split(':')[0] + '@s.whatsapp.net'
-  const botAdmin = participants.find(p => p.id.split(':')[0] + '@s.whatsapp.net' === botId)?.admin
-  const senderAdmin = participants.find(p => p.id.split(':')[0] + '@s.whatsapp.net' === senderId)?.admin
+> ₊· ⫏⫏ ㅤ Sσℓσ єη gяυρσѕ
+`.trim())
 
-  if (!senderAdmin) return m.reply('> ₊· ⫏⫏ ㅤ *ѕσℓσ α∂мιηѕ ρυє∂єη υѕαя єѕтє ¢σмαη∂σ*')
-  if (!botAdmin) return m.reply('> ₊· ⫏⫏ ㅤ *єℓ вσт ηє¢єѕιтα ѕєя α∂мιη*')
+  if (!isAdmin && !isOwner && !isROwner) return m.reply(`
+ㅤ    ꒰  ㅤ ❌ ㅤ *αℓуα ѕυв* ㅤ ⫏⫏  ꒱
+ㅤ    ⿻ ㅤ ✿ ㅤ α∂мιη 木 яєqυєяι∂σ ㅤ 性
+
+> ₊· ⫏⫏ ㅤ Nєcєѕιтαѕ ѕєя α∂мιη
+`.trim())
+
+  if (!isBotAdmin) return m.reply(`
+ㅤ    ꒰  ㅤ ❌ ㅤ *αℓуα ѕυв* ㅤ ⫏⫏  ꒱
+ㅤ    ⿻ ㅤ ✿ ㅤ вσт 木 ѕιη α∂мιη ㅤ 性
+
+> ₊· ⫏⫏ ㅤ Eℓ вσт ηє¢єѕιтα ѕєя α∂мιη
+`.trim())
 
   await conn.groupSettingUpdate(m.chat, 'announcement')
 
-  await m.reply(`
+  await conn.sendMessage(m.chat, {
+    text: `
 ㅤ    ꒰  ㅤ 🔒 ㅤ *αℓуα ѕυв* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ gяυρσ 木 ¢єяяα∂σ ㅤ 性
 
@@ -25,7 +35,17 @@ const handler = async (m, { conn }) => {
 
 ㅤ    ꒰  ㅤ 🕸️ ㅤ *ℓүσηη* ㅤ ⫏⫏  ꒱
 > ₊· ⫏⫏ ㅤ ✿ 木 性 ㅤ αℓуα
-  `.trim())
+    `.trim(),
+    contextInfo: {
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: "120363407253203904@newsletter",
+        newsletterName: "αℓуα - ¢нαηηєℓ",
+        serverMessageId: 1
+      }
+    }
+  }, { quoted: m })
 
   await m.react('🔒')
 }
@@ -35,7 +55,5 @@ handler.tags = ['group']
 handler.command = ['close', 'cerrar']
 handler.desc = 'ᴄᴇʀʀᴀʀ ᴇʟ ɢʀᴜᴘᴏ ꜱᴏʟᴏ ᴀᴅᴍɪɴꜱ'
 handler.group = true
-handler.admin = true
-handler.botAdmin = true
 
 export default handler
